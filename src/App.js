23c2364a6b51d0d809850bb5943b0e2f9e9c374e
Jackson Zhang <91386299+justonemorecommit@users.noreply.github.com>
2021-11-13 18:58:11 -0500

    Initialized project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Card, Container, Grid } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Card>
+        <Grid container fluid>
+          <Grid item md={6}>
+            <img src="/images/image.jpeg" />
+          </Grid>
+          <Grid item md={6}></Grid>
+        </Grid>
+      </Card>
+    </Container>
   );
 }
 
